0715805fb81ed2c9cf44affccea2764e6299cca4
Вероника Вашуркина <67638959+VeronikaVashurkina@users.noreply.github.com>
2021-06-07 23:13:57 +0300

    Add files via upload

---
+++ Lab1MIS/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Lab1MIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Частоты и длины интервалов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выборка:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(748, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Теоритические частоты:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(29, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 204);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(276, 82);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(367, 204);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(752, 82);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(265, 204);
+            this.listBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(708, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(309, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сгенерировать выборку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 405);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(988, 26);
+            this.textBox1.TabIndex = 19;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0,1",
+            "0,2",
+            "0,3"});
+            this.comboBox1.Location = new System.Drawing.Point(108, 332);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 28);
+            this.comboBox1.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(379, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = " T:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "100",
+            "50",
+            "90",
+            "80"});
+            this.comboBox2.Location = new System.Drawing.Point(427, 332);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(187, 28);
+            this.comboBox2.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 20);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "a:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 485);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Lab1MIS/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab1MIS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public long factorial(int num)
+        {
+            long res = 1;
+            for (int i = num; i > 1; i--)
+                res *= i;
+            return res;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            String b = (String)comboBox1.SelectedItem;
+           double a = double.Parse(b);
+
+            String c = (String)comboBox2.SelectedItem;
+            int T = int.Parse(c);
+
+            int n = 150;
+
+            
+            int[] mass = new int[n];
+            double y = 0;
+            double xi = 0;
+            
+           
+            bool flag = false;
+
+            Random rand = new Random();
+            for (int j = 0; j < n; j++)
+            {
+                while ((flag != true) && (y <= T))
+                {
+                    
+                    xi = (-1.0 / a) * Math.Log(rand.NextDouble());
+                    
+                    if ((y + xi) > T)
+                    {
+                        flag = true;
+                    }
+                    
+                    else
+                    {
+                        mass[j]++;
+                        y += xi;
+                    }
+
+                    xi = 0;
+                }
+                flag = false;
+                y = 0;
+            }
+            var ilist = new List<int>();
+            ilist.AddRange(mass);
+            ilist.Sort();
+
+            
+            listBox1.Items.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                listBox1.Items.Add(ilist[i]);
+            }
+
+            var newilist = new List<int>(ilist.Distinct());
+            var chastota = new List<int>();
+            int k = 0;
+           
+            for (int i = 0; i < newilist.Count; i++)
+            {
+                chastota.Add(0);
+                while ((k < ilist.Count) && (ilist[k] == newilist[i]))
+                {
+                    k++;
+                    chastota[i] = chastota[i] + 1;
+                }
+            }
+
+            
+            listBox2.Items.Clear();
+            for (int i = 0; i < chastota.Count; i++)
+            {
+                listBox2.Items.Add(i + 1 + ")   " + chastota[i]);
+            }
+
+            double tchastota = 0;
+            double ati = 0;
+            long fact = 0;
+            double exp = 0;
+            double atiexp = 0;
+
+            var teorchastota = new List<double>();
+            exp = Math.Pow(Math.E, (-(a * T)));
+            for (int i = 0; i < newilist.Count; i++)
+            {
+                ati = Math.Pow((a * T), newilist[i]);
+                fact = factorial(newilist[i]);
+                atiexp = (ati * exp);
+                tchastota = atiexp / (double)fact;
+                tchastota *= newilist.Count;
+                teorchastota.Add(tchastota * 10);
+                tchastota = 0; ati = 0; fact = 0;
+            }
+
+
+            listBox3.Items.Clear();
+            for (int i = 0; i < teorchastota.Count; i++)
+            {
+                listBox3.Items.Add(i + 1 + ") " + (int)teorchastota[i]);
+            }
+
+            
+            double xi2 = 0;
+            for (int i = 0; i < teorchastota.Count; i++)
+            {
+                xi2 += Math.Pow((double)chastota[i] - teorchastota[i], 2) / teorchastota[i];
+
+            }
+            if ((xi2 < 23.2)&&(xi2>0))
+            { textBox1.Text = "Критерий Пирсона в пределах нормы, равен:"+xi2+". Гипотеза о Пуассоновском распределении принимается."; }
+            else { textBox1.Text = "Критерий Пирсона  равен:" + xi2; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
